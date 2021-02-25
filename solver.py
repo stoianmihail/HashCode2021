308a36db676a1058b551e013a67f56fc0c101f01
Mihail Stoian <mihail.stoian@in.tum.de>
2021-02-25 20:10:09 +0100

    added parser

--- solver.py
+++ solver.py
@@ -2,7 +2,7 @@ import numpy as np
 import operator
 import sys
 
-files = ["a_", "b_"]
+files = ["a", "b", "c", "d", "e", "f"]
 
 def solve(file):
 	# Read each file
@@ -12,24 +12,43 @@ def solve(file):
 		content = f.read().splitlines()
 	
 	# Get first line
-	N, M, lines = list(map(int, content[0].split(' ')))
+	D, I, S, V, F = list(map(int, content[0].split(' ')))
 	
 	# Get array from the second line
 	print("Parsing..")
-	array = list(map(int, content[1].split(' ')))
-	pos = 2
-	for i in range(lines):
+	pos = 1
+	streets = list()
+	mapping = dict()
+	for i in range(S):
 		# Read the next lines
-		n = list(map(int, content[pos].split(' ')))
+		splitted = content[pos].split(' ')
+		B = int(splitted[0])
+		E = int(splitted[1])
+		name = splitted[2]
+		mapping[name] = i
+		L = int(splitted[3])
+		streets.append((B, E, name, L))
 		pos += 1
-	
+	cars = list()
+	for i in range(V):
+		splitted = content[pos].split(' ')
+		p = int(splitted[0])
+		
+		tmp = []
+		for j in range(p):
+			tmp.append(mapping[splitted[1 + j]])
+		cars.append(tmp)
+		pos += 1
 	score = 0
+	
+	print("streets=" + str(streets))
+	print("cars=" + str(cars))
 	
 	# solution = set()
 	print("Writing..")
-	with open("output/" + file.split('/')[1].replace(".in", "") + ".out", 'w+') as f:
-		f.write(str(lines) + "\n")
-		for i in range(lines):
+	with open("output/" + file.split('/')[1].replace(".txt", "") + ".out", 'w+') as f:
+		f.write(str(0) + "\n")
+		for i in range(0):
 			f.write("test" + "\n")
 	return score
 		
@@ -38,7 +57,7 @@ def main():
 	if len(sys.argv) == 1:
 		total = 0
 		for file in files:
-			total += solve("input/" + file + ".in")
+			total += solve("input/" + file + ".txt")
 		print("Total score: " + str(total))
 	else:
 		# Only one!
